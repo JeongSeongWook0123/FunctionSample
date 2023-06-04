9f4feec19a2cf343de08f5b79cee6b0f04b638c5
SeongUkJeong0123 <rkttgh@naver.com>
2023-06-04 22:18:48 +0900

    23.06.04 MainActivity 페이지 연동

--- app/src/main/java/com/jsu/functionapp/MainActivity.kt
+++ app/src/main/java/com/jsu/functionapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.jsu.functionapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.jsu.functionapp.databinding.ActivityMainBinding
+import com.jsu.functionapp.recyclersample.RecyclerViewSample01Activit
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,10 +29,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun adtSetting() {
 
-        val arrayAdtName = arrayListOf<String>("Recycler View Sample", "Dialog Sample")
-
+        val arrayAdtName = arrayListOf<String>("Recycler View Sample", "Dialog Sample",
+            "Test Sample01","Test Sample02","Test Sample03","Test Sample04")
         adtCard = MainCardAdapter(activity,arrayAdtName)
         binding.rvList.adapter = adtCard
+        binding.rvList.layoutManager = GridLayoutManager(activity,3)
 
     }
 
--- app/src/main/java/com/jsu/functionapp/MainCardAdapter.kt
+++ app/src/main/java/com/jsu/functionapp/MainCardAdapter.kt
@@ -1,16 +1,22 @@
 package com.jsu.functionapp
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.jsu.functionapp.databinding.ListviewCardViewMainBinding
 import com.jsu.functionapp.databinding.ListviewRecyclerSampleBinding
+import com.jsu.functionapp.dialogSample.DialogSample01Activity
+import com.jsu.functionapp.recyclersample.RecyclerViewSample01Activit
 
 
 class MainCardAdapter(
     private var context: Context,
-    private var arrayCardName: ArrayList<String>
+    private var arrayCardName: ArrayList<String>,
+    private var intent : Intent? = null
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -28,6 +34,24 @@ class MainCardAdapter(
 
                 holder.tvName.text = arrayCardName[position]
 
+                holder.view.setOnClickListener {
+                    when(arrayCardName[position].split(" ")[0]){
+                        "Recycler" -> {
+                            intent = Intent(context,RecyclerViewSample01Activit::class.java)
+                            context.startActivity(intent)
+                        }
+                        "Dialog" -> {
+                            intent = Intent(context,DialogSample01Activity::class.java)
+                            context.startActivity(intent)
+                        }
+                        else -> {
+                            Toast.makeText(context,"기능 개발 중 입니다.",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+
+
             }
         }
 
@@ -35,9 +59,7 @@ class MainCardAdapter(
 
     override fun getItemCount(): Int = arrayCardName.size
 
-    override fun getItemViewType(position: Int): Int {
-        return position
-    }
+    override fun getItemViewType(position: Int): Int = position
 
     inner class SampleListHolder(itemView: ListviewCardViewMainBinding) : RecyclerView.ViewHolder(itemView.root) {
 
